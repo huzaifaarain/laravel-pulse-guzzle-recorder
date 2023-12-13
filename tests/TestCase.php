59c2d569a51c8cd8f8a4fdde13bc6c8feff18f40
huzaifaarain <huzaifaarain@users.noreply.github.com>
2023-12-13 11:01:37 +0000

    Fix styling

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -3,8 +3,8 @@
 namespace MuhammadHuzaifa\LaravelPulseGuzzleRecorder\Tests;
 
 use Illuminate\Database\Eloquent\Factories\Factory;
-use Orchestra\Testbench\TestCase as Orchestra;
 use MuhammadHuzaifa\LaravelPulseGuzzleRecorder\LaravelPulseGuzzleRecorderServiceProvider;
+use Orchestra\Testbench\TestCase as Orchestra;
 
 class TestCase extends Orchestra
 {
